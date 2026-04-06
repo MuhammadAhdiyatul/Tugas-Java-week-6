06f38a745a613b64261d8963e64d7d2ff528a983
MuhammadAhdiyatul <muhammad.ahdiyatul.tif425@polban.ac.id>
2026-04-06 18:23:31 +0700

    mengerjakan nomor 1

---
+++ soal1/Employee.java
@@ -0,0 +1,28 @@
+package soal1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Employee {
+    private String name;
+    private int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{name='" + name + "', salary=" + salary + "}";
+    }
+}
